01841c535003f5cd4a584ce5f4a8895061669edb
Jen Fox <v-jenfox@microsoft.com>
2018-11-27 16:04:27 -0800

    Updated code based on Code Review

--- PartyCircuits/PartyCircuits.ino
+++ PartyCircuits/PartyCircuits.ino
@@ -1,54 +1,52 @@
-/* ----------------------------------------------------------------------------
-   Party Circuits Code for use with the Party Circuits lesson plan 
-   available from Microsoft Education Workshop at http://aka.ms/hackingSTEM 
- 
-   This project uses an Arduino UNO microcontroller board. More information can
-   be found by visiting the Arduino website: 
-   https://www.arduino.cc/en/main/arduinoBoardUno 
-  
-  This project involves one or more LEDs connected into the Arduino Digital inputs.
-  The lights are controlled via the Excel workbook.
- 
-  Comments, contributions, suggestions, bug reports, and feature requests 
-  are welcome! For source code and bug reports see: 
-  http://github.com/[TODO github path to Hacking STEM] 
-
-  Jen Fox, 2018 Microsoft Education Workshop
-  For issues with this code visit: https://aka.ms/hackingstemsupport
- 
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files (the "Software"), to deal
-  in the Software without restriction, including without limitation the rights to
-  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
-  of the Software, and to permit persons to whom the Software is furnished to do
-  so, subject to the following conditions:
-
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-
-  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
-  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE 
-  SOFTWARE. 
+/* -------------------__ Hacking STEM PartyCircuits Arduino __-----------------===//
+//   For use with the Party Circuits lesson plan 
+//   available from Microsoft Education Workshop at
+//   http://aka.ms/hackingSTEM 
+//
+//  Overview:
+//  This project flashes one or more LEDs based on commands sent from the Excel 
+//  Party Circuits workbook. Commands (intensity, flash speed, and which lights
+//  are on) are send as Hex numbers in 9-element array. 
+//  To test commands in the Arduino Serial Monitor, use this example:
+//        0,3EC,2C4,364,154,374,248,3A8,2BC
+//  Pins:
+//  Digital Pin 3 -- LED 1
+//  Digital Pin 5 -- LED 2
+//  Digital Pin 6 -- LED 3
+//  Digital Pin 9 -- LED 4
+//  Digital Pin 10 -- LED 5
+//  Digital Pin 11 -- LED 6
+// 
+//  This project uses an Arduino UNO microcontroller board, information at:
+//  https://www.arduino.cc/en/main/arduinoBoardUno
+//
+//  Comments, contributions, suggestions, bug reports, and feature requests
+//  are welcome! For source code and bug reports see:
+//  http://github.com/[TODO github path to Hacking STEM]
+//
+//  Copyright 2018 Jen Fox Microsoft EDU Workshop - HackingSTEM
+//  MIT License terms detailed in LICENSE.txt 
 ----------------------------------------------------------------------------
 */ 
 
-// Sample Command for Testing
-// 0,3EC,2C4,364,154,374,248,3A8,2BC
-
 #include <String.h>
-// ----------------------------------------------------------
-// Program variables 
-// ----------------------------------------------------------
+// -------------------------------------------------------------------------
+//                      PROGRAM VARIABLES
+// -------------------------------------------------------------------------
 //Total number of LEDs (max. of 6)
-int kNumberOfLeds = 6;
+const int kMaxNumberOfLeds = 6; //This is the maximum number of LEDs we can add
 //  Create an array to hold the LED pins
-int ledArray[] = {3, 5, 6, 9, 10, 11};
+const int kLedArray[] = {3, 5, 6, 9, 10, 11};
 //  Duration that LEDs are OFF (in milliseconds)
-const int ledTimeOff = 50;
+const int kLedTimeOff = 50;
+
+// Variables for LED intensity and Flash Speed. Can adjust as desired.
+const int kLedBright = 255;
+const int kLedMed = 200;
+const int kLedDim = 100;
+const int kFastSpeed = 250;
+const int kMedSpeed = 500;
+const int kSlowSpeed = 1000;
 
 //Declare variables for Excel commands
 bool priorLoopValue; 
@@ -59,14 +57,9 @@ int flashSpeed;
 //   LED status (0 = off, 1 = on)
 int ledStatusArray[6] = {};
 
-// Variables for LED intensity and Flash Speed. Can adjust as desired.
-int ledBright = 255;
-int ledDim = 100;
-int fastSpeed = 250;
-int medSpeed = 500;
-int slowSpeed = 1000;
-
-// Bitwise operator variables 
+// Bitwise operator variables to extract Excel data. 
+//   This is an advanced programming concept, 
+//   leave as-is if unfamiliar with bitwise operations
 int bitwiseArray[8][2] =
 {{0x300, 8},
 {0xC0, 6},
@@ -84,11 +77,13 @@ int loopTrack = 0; //variable to send to Excel to keep track of loop iterations
 float currentCommand = 0; //variable to store current command
 float priorCommand; //variable to store prior command
 
-float incomingExcelFloat = 0; // Command Trigger 
+// Command Loop Variable
+bool isPatternLooped = 0; 
           
 // Serial data variables ------------------------------------------------------
 // IMPORTANT: This must be equal to number of channels set in Data Streamer
 const byte kNumberOfChannelsFromExcel = 9; //Incoming Serial Data Array
+const int kNumberOfCommands = 8; //Incoming Command Array
 
 String ledSettingArray[kNumberOfChannelsFromExcel][10];
 
@@ -100,11 +95,13 @@ boolean stringComplete = false;   // Indicates complete string (newline found)
 const int kSerialInterval = 50;   // Interval between serial writes
 unsigned long serialPreviousTime; // Timestamp to track serial interval
 
-// SETUP ----------------------------------------------------------------------
+// --------------------------------------------------------------------
+//                               SETUP 
+// --------------------------------------------------------------------
 void setup() {
   // Initialize led pins as outputs! 
-  for(int i; i < kNumberOfLeds; i++){
-    pinMode(ledArray[i], OUTPUT);
+  for(int i; i < kMaxNumberOfLeds; i++){
+    pinMode(kLedArray[i], OUTPUT);
   }
  
   //Initialize the serial port
@@ -116,36 +113,25 @@ void loop()
 {
   // Read Excel variables from serial port (Data Streamer)
   processIncomingSerial();
-  delay(1000); // This is just for debugging
   
   for (int i = 0; i < kNumberOfChannelsFromExcel; i++)
   {
     parseHexValues(i+1);
-    flashLeds(i);
     processOutgoingSerial();
-    // Need to debug? Print the ledHexArray to determine if it matches the Excel commands
-    // for (int j = 0; j < 8; j++){
-    // Serial.print(ledHexArray[j], DEC);
-    // Serial.print(",");
-    // }
-    // Serial.println();
+    flashLeds(i);
   }
-//More debugging! Uncomment if necessary
-  // for (int i = 0; i < 8; i++){
-  //   Serial.println(ledHexArray[i], BIN);
-  // }
-
 }
-//-------------------------------------------------------------------
-// Party Circuits Functions
-//-------------------------------------------------------------------
 
+//-------------------------------------------------------------------
+//                      Party Circuits Functions
+//-------------------------------------------------------------------
+// Take Excel commands in Hex and separate into individual commands in binary string
 void parseHexValues(int index)
 {
-    char stringCopy[incomingSerialData[index].length()+1];   // Array to chars 
+    char stringCopy[incomingSerialData[index].length()+1];   // 
     incomingSerialData[index].toCharArray(stringCopy, incomingSerialData[index].length()+1); // Convert String object to char[]
     int hex = strtol(stringCopy, NULL, 16); // string to long converts str to string w/ any base
-    for (int i = 0; i < 8; i++){
+    for (int i = 0; i < kNumberOfCommands; i++){
       int val = hex & bitwiseArray[i][0]; // filter out all bits not relevant for command
       val = val >> bitwiseArray[i][1]; // shift all bits to get relevant command
       ledHexArray[i] = val;
@@ -155,35 +141,33 @@ void parseHexValues(int index)
 // Excel-driven LED Flashing Function
 void flashLeds(int commandNum){
     // Separate each column of incomingSerialData into separate commands
-       intensityRaw= ledHexArray[0];
-       flashSpeedRaw = ledHexArray[1];
-       for(int i = 0; i < kNumberOfLeds; i++){
+       for(int i = 0; i < kMaxNumberOfLeds; i++){
         ledStatusArray[i] = ledHexArray[i+2];
        }
 
        //Determine intensity and LED flash speed from Excel Commands
-       intensity = ledIntensity(intensityRaw);
-       flashSpeed = ledSpeed(flashSpeedRaw);
+       intensity = ledIntensity(ledHexArray[0]);
+       flashSpeed = ledSpeed(ledHexArray[1]);
 
       //Determine command number based on input 
        commandNumber = commandNum;
       
        // Flash appropriate LEDs at given intensity       
-       for(int i = 0; i < kNumberOfLeds; i++){
+       for(int i = 0; i < kMaxNumberOfLeds; i++){
         if(ledStatusArray[i] == 1){
-          analogWrite(ledArray[i], intensity);
+          analogWrite(kLedArray[i], intensity);
         } 
-        else(analogWrite(ledArray[i], LOW));
+        else(analogWrite(kLedArray[i], LOW));
        }
        
        delay(flashSpeed);
 
        //Turn off all LEDs
-       for(int i = 0; i < kNumberOfLeds; i++){
-          analogWrite(ledArray[i], LOW);
+       for(int i = 0; i < kMaxNumberOfLeds; i++){
+          analogWrite(kLedArray[i], LOW);
         } 
        
-       delay(ledTimeOff); //same off duration for all sequences
+       delay(kLedTimeOff); //same off duration for all sequences
      
   // Keep track of number of times command sequence is repeated
   loopTrack++;
@@ -199,68 +183,36 @@ bool isLoop(int loopCommand){
   } 
 }
 
-// Check value of LED intensity
+// Check and return LED intensity
 int ledIntensity(int intensity){
   if(intensity == 1){
-    return ledDim;
+    return kLedDim;
   }
   else if(intensity == 2){
-    return ledBright;
+    return kLedMed;
+  }
+  else if(intensity == 3){
+    return kLedBright;
   }
   else{
     return 0;
   }
 }
 
-// Check and determine speed
+// Check and return LED flash speed
 int ledSpeed(int flashSpeed){
   if(flashSpeed == 1){
-    return slowSpeed;
+    return kSlowSpeed;
   }
   else if(flashSpeed == 2){
-    return medSpeed;
-  }
-  else if(flashSpeed == 3){
-    return fastSpeed;
+    return kMedSpeed;
   }
   else{
-    return fastSpeed;
+    return kFastSpeed;
   }
 }
 
-void parseLedSettings()
-{
-  for (int i = 0; i < kNumberOfChannelsFromExcel; i++){
-    char stringCopy[incomingSerialData[i+1].length()+1];
-    incomingSerialData[i+1].toCharArray(stringCopy, incomingSerialData[i+1].length()+1);
-    char *token = strtok(stringCopy, ",");
-    int j = 0;
-    while (token != NULL){
-      ledSettingArray[i][j] = token;
-      token = strtok(NULL, ",");
-      j++;
-    }
-  }
-}
-
-//Function for specialized string search: go through a string and pull out characters
-String getValue(String dataString, char separator, int index)
-{                                           // basic searching algorithm
-                                            // data is the serial string, separator is a comma, index is where we want to look in the data array
-  int matchingIndex = 0;                    // no match because we are starting to look
-  int strIndex[] = {0, -1};
-  int maxIndex = dataString.length()-1;
-  for(int i=0; i<=maxIndex && matchingIndex<=index; i++){     // loop until end of array or until we find a match
-    if(dataString.charAt(i)==separator || i==maxIndex){             // if we hit a comma OR we are at the end of the array
-      matchingIndex++;                                        // increment matchingIndex to keep track of where we have looked
-      strIndex[0] = strIndex[1]+1;                            // set substring parameters
-           // ternary operator in objective c is [condition] ? [true expression] : [false expression] 
-      strIndex[1] = (i == maxIndex) ? i+1 : i;
-    }
-  }
-  return matchingIndex>index ? dataString.substring(strIndex[0], strIndex[1]) : ""; // if match return substring or else return ""
-}
-
+// 
 // INCOMING SERIAL DATA PROCESSING CODE----------------------------------------
 // Process serial data inputString from Data Streamer
 void ParseSerialData()
@@ -271,13 +223,11 @@ void ParseSerialData()
 
     // Set variables based on array index referring to columns:
     // Data Out column A5 = 0, B5 = 1, C5 = 2, etc.
-    incomingExcelFloat = incomingSerialData[0].toFloat(); // Data Out column A5
-    //incomingExcelString1 = incomingSerialData[1]; // Data Out column B5
+    isPatternLooped = incomingSerialData[0].toFloat(); // First cell in Data Out Sheet 
        
     inputString = ""; // reset inputString
     stringComplete = false; // reset stringComplete flag
   }
-
 }
 
 // OUTGOING SERIAL DATA PROCESSING CODE----------------------------------------
@@ -289,7 +239,7 @@ void sendDataToSerial()
   Serial.print(kDelimiter);
   
   // Example test for incoming Excel variables
-  Serial.print(incomingExcelFloat);
+  Serial.print(isPatternLooped);
   Serial.print(kDelimiter);
 
   //LED status (on/off)
@@ -303,6 +253,9 @@ void sendDataToSerial()
   Serial.print(kDelimiter);
   Serial.print(ledHexArray[5]);
   Serial.print(kDelimiter);
+  Serial.print(ledHexArray[6]);
+  Serial.print(kDelimiter); 
+  Serial.print(ledHexArray[7]);
     
   Serial.println(); // Add final line ending character only once
 }
@@ -329,7 +282,7 @@ void processIncomingSerial()
   ParseSerialData();
 }
 
-// Gathers bits from serial port to build inputString
+// Gathers bytes from serial port to build inputString
 void getSerialData(){
   if(Serial.available()){
     inputString = Serial.readStringUntil('\n');
